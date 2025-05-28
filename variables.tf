0c4cbec248a8a58161a3ae67e25628662549c8b1
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-28 16:32:05 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "gcp_creds" {
+  type        = string
+  description = "GCP service account credentials (JSON)"
+  sensitive   = true
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP zone"
+  default     = "us-central1-a"
+}
