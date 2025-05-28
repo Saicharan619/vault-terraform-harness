09b115f30fcc08c417a41aa547dee5bd2eab5a8d
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-28 18:57:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,33 @@
-variable "region" {
+variable "gcp_creds" {
+  description = "Base64 encoded GCP service account key from Vault"
   type        = string
-  description = "GCP region"
+  sensitive   = true
+}
+
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
   default     = "us-central1"
 }
 
 variable "zone" {
+  description = "GCP Zone"
   type        = string
-  description = "GCP zone"
   default     = "us-central1-a"
 }
 
 variable "vault_address" {
+  description = "Vault address (VAULT_ADDR env)"
   type        = string
-  description = "Vault server URL (e.g., http://34.93.44.120:8200)"
 }
 
 variable "vault_token" {
+  description = "Vault token (VAULT_TOKEN env)"
   type        = string
-  description = "Vault token with access to secret/gcpcred"
   sensitive   = true
 }
