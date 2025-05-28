121b0b193f1d54e2adf43cb12e73a917bb2cca07
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-28 19:17:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+variable "vault_address" {}
+variable "vault_token" {}
+
+variable "region" {
+  default = "us-central1"
+}
+variable "zone" {
+  default = "us-central1-a"
+}
+
+provider "vault" {
+  address = var.vault_address
+  token   = var.vault_token
+}
+
+data "vault_generic_secret" "gcp_creds" {
+  path = "secret/data/gcpcreds"  # Your Vault secret path here
+}
+
 provider "google" {
   credentials = base64decode(data.vault_generic_secret.gcp_creds.data["servicekey_base64"])
   project     = data.vault_generic_secret.gcp_creds.data["project_id"]
